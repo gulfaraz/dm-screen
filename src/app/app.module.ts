8806271d645d8594e6cb2035a688d51e636f5965
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2023-04-22 18:25:09 +0200

    feat: add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicModule } from '@ionic/angular';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import {
+    provideAnalytics,
+    getAnalytics,
+    ScreenTrackingService,
+} from '@angular/fire/analytics';
 
 import { AppRoutingModule } from './app-routing.module';
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { DMScreenComponent } from './dm-screen.component';
 
@@ -11,9 +18,11 @@ import { DMScreenComponent } from './dm-screen.component';
         BrowserModule,
         IonicModule.forRoot({ mode: 'ios' }),
         AppRoutingModule,
+        provideFirebaseApp(() => initializeApp(environment.firebase)),
+        provideAnalytics(() => getAnalytics()),
     ],
     declarations: [AppComponent, DMScreenComponent],
     bootstrap: [AppComponent],
-    providers: [],
+    providers: [ScreenTrackingService],
 })
 export class AppModule {}
