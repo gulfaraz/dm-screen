b6aab48d8036d75b779d049a6aa9ae81faad5a94
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2023-05-07 17:44:33 +0200

    feat: remove firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicModule } from '@ionic/angular';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import {
-    provideAnalytics,
-    getAnalytics,
-    ScreenTrackingService,
-} from '@angular/fire/analytics';
 
 import { AppRoutingModule } from './app-routing.module';
-import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { DMScreenComponent } from './dm-screen.component';
 
@@ -18,11 +11,8 @@ import { DMScreenComponent } from './dm-screen.component';
         BrowserModule,
         IonicModule.forRoot({ mode: 'ios' }),
         AppRoutingModule,
-        provideFirebaseApp(() => initializeApp(environment.firebase)),
-        provideAnalytics(() => getAnalytics()),
     ],
     declarations: [AppComponent, DMScreenComponent],
     bootstrap: [AppComponent],
-    providers: [ScreenTrackingService],
 })
 export class AppModule {}
